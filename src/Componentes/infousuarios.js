006cbdaa78fc3d8956bb435dc53902ed446862d1
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-10-11 16:00:32 -0300

    Aula 31, difícil mas o progresso é uma constancia

---
+++ src/Componentes/infousuarios.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default CardVideo;
+
+function CardVideo(){
+    const reproduzVideo = () => {
+        alert("O video está sendo reproduzido");
+    }
+
+function InfoUsuarios(){
+    return(
+        <div>
+            Autor: Marcelo Castro
+        </div>
+    )
+    }}
